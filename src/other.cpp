bb7bfec668e0f22cf22b4b5b8e8528334868080a
zhanhb <6323014+zhanhb@users.noreply.github.com>
2023-02-16 16:31:18 +0800

    Create other.cpp

---
+++ src/other.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+// <editor-fold defaultstate="collapsed" desc="#define out(...)">
+#ifndef out
+#define out(...)
+#endif
+// </editor-fold>
+
+using namespace std;
+
+// <editor-fold defaultstate="collapsed" desc="unknown">
+
+// </editor-fold>
+

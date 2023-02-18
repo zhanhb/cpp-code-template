4359530a89b4f900547a628ff83ef83bff060793
zhanhb <6323014+zhanhb@users.noreply.github.com>
2023-02-18 15:24:10 +0800

    tree-node: add ostream::sentry, width support

--- include/leetcode/tree-node.h
+++ include/leetcode/tree-node.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <queue>
+#include <sstream>
 
 struct TreeNode {
     int val;
@@ -16,6 +17,16 @@ struct TreeNode {
 
 template<class CharT, class Traits>
 inline std::basic_ostream<CharT, Traits> &operator<<(std::basic_ostream<CharT, Traits> &out, const TreeNode *root) {
+    std::streamsize width = out.width(0);
+    if (width) {
+        std::stringstream ss;
+        ss.copyfmt(out);
+        ss << root;
+        out.width(width);
+        return out << ss.str();
+    }
+    typename std::basic_ostream<CharT, Traits>::sentry sentry(out);
+    if (!sentry) return out;
     if (!root) return out << "[]";
     char ch = '[';
     std::queue<const TreeNode *> q;

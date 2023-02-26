8e6ad8c9dea8d6e49b7f491dc9b212f7a5ee8c2d
zhanhb <6323014+zhanhb@users.noreply.github.com>
2023-02-26 10:54:24 +0800

    hash-tuple: check tuple element hashable

--- include/misc/hash-tuple.h
+++ include/misc/hash-tuple.h
@@ -8,8 +8,48 @@
 #include <tuple>
 #include <type_traits>
 
+namespace hash_tuple_detail {
+
+#if __cplusplus >= 201703L
+    using std::void_t;
+#else
+    template<class...>
+    struct void_t_impl {
+        typedef void type;
+    };
+    template<class ...Args> using void_t = typename void_t_impl<Args...>::type;
+#endif
+
+    template<class Key, class = std::hash<Key>, class = void>
+    struct has_enabled_hash : std::false_type {
+    };
+    template<class Key, class Hash>
+    struct has_enabled_hash<Key, Hash, void_t<
+            // to avoid implicit instantiation of undefined template
+            decltype(size_t(Hash()(std::declval<Key const &>())))
+    > > : std::integral_constant<bool,
+            std::is_copy_constructible<Hash>::value &&
+            std::is_move_constructible<Hash>::value &&
+            std::is_default_constructible<Hash>::value &&
+            std::is_destructible<Hash>::value
+    > {
+    };
+
+    template<class Type, class>
+    using enable_hash_helper_imp = Type;
+
+    template<class Type, class ...Keys>
+    using enable_hash_helper = enable_hash_helper_imp<Type,
+            void_t<typename std::enable_if<has_enabled_hash<Keys>::value>::type...>
+    >;
+}
+
 template<class First, class ...Rest>
-class std::hash<std::tuple<First, Rest...> > {
+class std::hash<hash_tuple_detail::enable_hash_helper<
+        std::tuple<First, Rest...>,
+        typename std::remove_const<First>::type,
+        typename std::remove_const<Rest>::type...
+> > {
 
     typedef std::tuple<First, Rest...> target_type;
     typedef std::tuple_size<target_type> tuple_size;
